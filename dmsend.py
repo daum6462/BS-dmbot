e8078f9b6ffca9a71be521057c738a1218fa8754
daum6462 <89126061+daum6462@users.noreply.github.com>
2021-11-12 12:19:34 +0900

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -4,13 +4,13 @@ import datetime
 
 client = discord.Client()
 
-__TOKEN__ = '★봇토큰★'
-titlke = '★임베드 제목 입력★'
+__TOKEN__ = 'OTA4NTUxMTM2MDYyMDg3MTY5.YY3YLg.SyEwj-R1zKL6CLym-wg9Vql9dKg'
+titlke = '보안최강 AINWIN'
 
 @client.event
 async def on_ready():
-    print(client.user)
-    #game = discord.Game('상태메시지')
+    print("봇이 정상상적으로 실행 되었습니다")
+    #game = discord.Game('뒷메돌리기')
     #await client.change_presence(status=discord.Status.online, activity=game)
 
 #!디엠 {할말}
@@ -23,8 +23,10 @@ async def on_message(message):
             else:
                 try:
                     msg = message.content[4:]
-                    if message.author.guild_permissions.manage_messages:
-                        embed = discord.Embed(title=tile, description=msg, colour=discord.Colour.gold(), timestamp=message.created_at)
+                    if message.author.id ==897471888043937792:
+                 if message.author.guild_permissions.manage_messages:897471888043937792
+                        embed = discord.Embed(title=tile, description=msg, colour=discord.Colour.gold(), timestamp=message.created_at, title="보안최강 AINWIN
+                        embed .add_field
                         #embed.set_footer(text="맨 밑에 들어갈 내용")
                         await i.send(embed=embed)
                         print(f'{i} 전송')
@@ -32,4 +34,4 @@ async def on_message(message):
                     pass
 
 
-client.run(__TOKEN__)
+client.run('OTA4NTUxMTM2MDYyMDg3MTY5.YY3YLg.SyEwj-R1zKL6CLym-wg9Vql9dKg')
